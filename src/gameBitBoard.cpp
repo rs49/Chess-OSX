534b103d34618ef2476990ca00800dadb2ddf64e
Radoslaw Sadowski <rsadowski49@Radoslaws-MacBook-Pro.local>
2017-06-29 17:11:39 -0400

    Added GameBitboard class implementation

---
+++ src/gameBitBoard.cpp
@@ -0,0 +1,94 @@
+#include "gameBitBoard.h"
+
+void GameBitBoard::update(list<Piece*> whitePieces, list<Piece*> blackPieces, int enPas)
+{
+	mWhiteBitBoard = 0;
+	mBlackBitBoard = 0;
+
+	list<Piece*>::iterator it;
+
+	int position64;
+
+	for( it=whitePieces.begin(); it !=whitePieces.end(); it++)
+	{
+		position64 = board120to64[ (*it)->getPosition() ];
+		mWhiteBitBoard |= ( uint64_t(1) << position64 );
+
+		if( (*it)->getPieceType() == KING )
+		{
+			mWhiteKingPosition120 = (*it)->getPosition();
+		}
+	}
+
+	for( it=blackPieces.begin(); it !=blackPieces.end(); it++)
+	{
+		position64 = board120to64[ (*it)->getPosition() ];
+		mBlackBitBoard |= ( uint64_t(1) << position64 );
+
+		if( (*it)->getPieceType() == KING )
+		{
+			mBlackKingPosition120 = (*it)->getPosition();
+		}
+	}
+
+	mBothBitBoards = mWhiteBitBoard | mBlackBitBoard;
+
+	mEnPas = enPas;
+}
+
+bool GameBitBoard::isTileFree(int position64)
+{
+	if( ( uint64_t(1) << position64 ) & mBothBitBoards )
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool GameBitBoard::isWhitePiece(int position64)
+{
+	if( ( uint64_t(1) << position64 ) & mWhiteBitBoard )
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool GameBitBoard::isBlackPiece(int position64)
+{
+	if( ( uint64_t(1) << position64 ) & mBlackBitBoard )
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+int GameBitBoard::getWhiteKing()
+{
+	return mWhiteKingPosition120;
+}
+
+int GameBitBoard::getBlackKing()
+{
+	return mBlackKingPosition120;
+}
+
+int GameBitBoard::getEnPas()
+{
+	return mEnPas;
+}
+
+void GameBitBoard::setEnPas(int square120)
+{
+	mEnPas = square120;
+}
+	
---
+++ src/gameBitBoard.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "piece.h"
+
+class GameBitBoard
+{
+	private:
+		uint64_t mWhiteBitBoard;
+		uint64_t mBlackBitBoard;
+		uint64_t mBothBitBoards;
+		int mWhiteKingPosition120;
+		int mBlackKingPosition120;
+		int mEnPas;
+
+	public:
+		void update(list<Piece*> whitePieces, list<Piece*> blackPieces, int enpas);
+		bool isTileFree(int position64);
+		bool isWhitePiece(int position64);
+		bool isBlackPiece(int position64);
+		int getWhiteKing();
+		int getBlackKing();
+		int getEnPas();
+		void setEnPas(int square120);
+};
+
