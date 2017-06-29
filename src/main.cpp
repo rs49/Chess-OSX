09e6d8477f7c32a308410a5209c492db6e2b7a46
Radoslaw Sadowski <rsadowski49@Radoslaws-MacBook-Pro.local>
2017-06-29 10:46:15 -0400

    Set up game loop

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,53 @@ int main()
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);	
 
+	SDL_Event e;
+
 	int run = 1;
 
-	//while(run)
-	//{
+	Uint32 startTick, elapsed;
+
+	while(run)
+	{
+		startTick = SDL_GetTicks();
+
+		// Parse Inputs/Events
+	
+		while( SDL_PollEvent(&e) != 0 )
+		{
+			if( e.type == SDL_QUIT )
+			{
+				run = 0;
+			}
+
+			if( e.type == SDL_KEYDOWN )
+			{
+				switch( e.key.keysym.sym )
+				{
+					case SDLK_q:
+						{
+							run = 0;
+							break;
+						}
+				}
+			}
+
+		}
+
+		// Draw the screen
+
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 		SDL_RenderClear(renderer);
 		SDL_RenderPresent(renderer);
 
-		SDL_Delay(2000);
-	//}
+		// Run at 30 frames per second
+
+		elapsed = SDL_GetTicks() - startTick;
+		if( elapsed < 33 )
+		{
+			SDL_Delay( 33 - elapsed );
+		}
+	}
 
 	return 0;
 
